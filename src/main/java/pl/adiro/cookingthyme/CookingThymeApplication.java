f4801f95843f6bf546e076d05e81ced7ce407ac5
xAdiro <adiro.rostek@gmail.com>
2023-06-08 01:56:47 +0200

    Initial commit

---
+++ src/main/java/pl/adiro/cookingthyme/CookingThymeApplication.java
@@ -0,0 +1,13 @@
+package pl.adiro.cookingthyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingThymeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookingThymeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/pl/adiro/cookingthyme/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package pl.adiro.cookingthyme.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public String homePage(){
+        return "home";
+    }
+}
